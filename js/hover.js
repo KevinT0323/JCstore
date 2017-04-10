9f6956a2e110599cc83248161ee96091c03074c4
Kevin <bgtrfvcde99@gmail.com>
2017-04-10 20:31:17 +0800

    Add files via upload

---
+++ js/hover.js
@@ -0,0 +1,37 @@
+$("#item1").mouseover(function(){
+	$("#item1").animate({
+		paddingRight: 50+'%'},
+		600);
+	$("#content1").animate({
+		right: 25+'%',
+		fontSize: 18+'px'},
+		600);
+});
+$("#item2").mouseover(function(){
+	$("#item2").animate({
+		paddingLeft: 50+'%'},
+		600);
+	$("#content2").animate({
+		left: 25+'%',
+		fontSize: 18+'px'},
+		600);
+});
+$("#item3").mouseover(function(){
+	$("#item3").animate({
+		paddingRight: 50+'%'},
+		600);
+	$("#content3").animate({
+		right: 25+'%',
+		fontSize: 18+'px',
+		color: 'white'},
+		600);
+});
+$("#item4").mouseover(function(){
+	$("#item4").animate({
+		paddingLeft: 50+'%'},
+		600);
+	$("#content4").animate({
+		left: 25+'%',
+		fontSize: 18+'px'},
+		600);
+});
